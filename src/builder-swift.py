cb90cfd1c75a694ac870f028eca17e1418b8d0b6
CodedOre <47981497+CodedOre@users.noreply.github.com>
2019-09-25 12:27:47 +0200

    Now resolving dependencies!
    Added PipelineStage for Dependencies.

--- src/builder-swift.py
+++ src/builder-swift.py
@@ -31,13 +31,13 @@ class SwiftBuildSystemDiscovery(Ide.SimpleBuildSystemDiscovery):
 
 class SwiftBuildSystem(Ide.Object, Ide.BuildSystem):
     project_file = GObject.Property(type=Gio.File)
-
+    
     def do_get_id(self):
         return 'swift'
-
+    
     def do_get_display_name(self):
         return 'Swift'
-
+    
     def do_get_priority(self):
         return 0
 
@@ -61,45 +61,57 @@ class SwiftPipelineAddin(Ide.Object, Ide.PipelineAddin):
 
         if not runtime.contains_program_in_path('swift'):
             raise OSError('The runtime must contain swift to build Swift projects')
-
+        
+        dependend_launcher = pipeline.create_launcher()
+        dependend_launcher.set_cwd(srcdir)
+        dependend_launcher.push_argv('swift')
+        dependend_launcher.push_argv('package')
+        dependend_launcher.push_argv('resolve')
+        
         build_launcher = pipeline.create_launcher()
         build_launcher.set_cwd(srcdir)
-        build_launcher.push_argv("swift")
+        build_launcher.push_argv('swift')
         build_launcher.push_argv('build')
-
+        
         clean_launcher = pipeline.create_launcher()
         clean_launcher.set_cwd(srcdir)
-        clean_launcher.push_argv("swift package")
+        clean_launcher.push_argv('swift')
+        clean_launcher.push_argv('package')
         clean_launcher.push_argv('clean')
-
+        
+        install_launcher = pipeline.create_launcher()
+        install_launcher.set_cwd(srcdir)
+        install_launcher.push_argv('swift')
+        install_launcher.push_argv('run')
+        
+        dependend_stage = Ide.PipelineStageLauncher.new(context, dependend_launcher)
+        dependend_stage.set_name(_("Resolving Dependencies"))
+        self.track(pipeline.attach(Ide.PipelinePhase.DEPENDENCIES, 0, dependend_stage))
+        
         build_stage = Ide.PipelineStageLauncher.new(context, build_launcher)
         build_stage.set_name(_("Building project"))
         build_stage.set_clean_launcher(clean_launcher)
         build_stage.connect('query', self._query)
         self.track(pipeline.attach(Ide.PipelinePhase.BUILD, 0, build_stage))
-
-        install_launcher = pipeline.create_launcher()
-        install_launcher.set_cwd(srcdir)
-        install_launcher.push_argv('swift')
-        install_launcher.push_argv('run')
+        
         install_stage = Ide.PipelineStageLauncher.new(context, install_launcher)
         install_stage.set_name(_("Installing project"))
         self.track(pipeline.attach(Ide.PipelinePhase.INSTALL, 0, install_stage))
-
+    
     def _query(self, stage, pipeline, targets, cancellable):
         stage.set_completed(False)
 
 class SwiftBuildTarget(Ide.Object, Ide.BuildTarget):
-
+    
     def do_get_install_directory(self):
         return None
-
+    
     def do_get_name(self):
         return 'swift-run'
-
+    
     def do_get_language(self):
         return 'swift'
-
+    
     def do_get_cwd(self):
         context = self.get_context()
         project_file = Ide.BuildSystem.from_context(context).project_file
@@ -107,28 +119,28 @@ class SwiftBuildTarget(Ide.Object, Ide.BuildTarget):
             return project_file.get_path()
         else:
             return project_file.get_parent().get_path()
-
+    
     def do_get_priority(self):
         return 0
 
 class SwiftBuildTargetProvider(Ide.Object, Ide.BuildTargetProvider):
-
+    
     def do_get_targets_async(self, cancellable, callback, data):
         task = Ide.Task.new(self, cancellable, callback)
         task.set_priority(GLib.PRIORITY_LOW)
-
+        
         context = self.get_context()
         build_system = Ide.BuildSystem.from_context(context)
-
+        
         if type(build_system) != SwiftBuildSystem:
             task.return_error(GLib.Error('Not swift build system',
                                          domain=GLib.quark_to_string(Gio.io_error_quark()),
                                          code=Gio.IOErrorEnum.NOT_SUPPORTED))
             return
-
+        
         task.targets = [build_system.ensure_child_typed(SwiftBuildTarget)]
         task.return_boolean(True)
-
+    
     def do_get_targets_finish(self, result):
         if result.propagate_boolean():
             return result.targets
